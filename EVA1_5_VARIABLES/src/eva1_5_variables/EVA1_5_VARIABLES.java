c1a4d9cf1df2743bde67512b46980b248e51cfee
sofiaanchondo <anchondosofia78@gmail.com>
2025-08-28 15:55:05 -0700

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author mikas
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Declarar variables 
+        //Tipo de dato  Nombre del identificador
+        String nombre;
+        int edad;
+        double  salario;
+        //Inicializacion
+        //Identificador = valor;
+        // --> Operador de asignacion 
+        nombre = "Sofia Anchondo Rodriguez";
+                edad = 17;
+                        salario= 1.1;
+                        System.out.println(nombre);
+                        System.out.println(edad);
+                        System.out.println(salario);
+                        
+                        
+                        
+        
+    }
+    
+}
