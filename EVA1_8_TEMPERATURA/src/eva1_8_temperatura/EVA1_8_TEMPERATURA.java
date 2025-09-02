3be58ee3f27ef2bfbb407a1db6deac28496baf9d
sofiaanchondo <anchondosofia78@gmail.com>
2025-09-01 20:16:04 -0700

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author mikas
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   double c, f;
+   f = 18;
+   c = 5 * (f - 32) /9;
+   System.out.println("Cambio de temperatura de 18 fahrenheits a celsius");
+   System.out.println(c);
+   
+   
+   
+   
+   
+    }
+    
+}
